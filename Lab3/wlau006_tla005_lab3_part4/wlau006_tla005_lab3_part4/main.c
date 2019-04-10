f519e3652675da322a20b5a3dc847340a335a3c4
Tinh La <tla005@ucr.edu>
2019-04-10 08:42:08 -0700

    Update main.c

--- Lab3/wlau006_tla005_lab3_part4/wlau006_tla005_lab3_part4/main.c
+++ Lab3/wlau006_tla005_lab3_part4/wlau006_tla005_lab3_part4/main.c
@@ -35,24 +35,25 @@ int main(void)
 		tmpA = PINA & 0xFF; // Read input off of PINA
 		tmpB = tmpA & 0x0F;
 		tmpC = tmpA & 0xF0;
-		// PA7 -> PB3
-		//tmpB = SetBit(tmpB, 3, GetBit(tmpA,7));
-		// PA6 -> PB2
-		//tmpB = SetBit(tmpB, 2, GetBit(tmpA,6));
-		// PA5 -> PB1
-		//tmpB = SetBit(tmpB, 1, GetBit(tmpA,5));
-		// PA4 -> PB0
-		//tmpB = SetBit(tmpB, 0, GetBit(tmpA,4));
-		// PA3 -> PC7
-		//tmpC = SetBit(tmpC, 7, GetBit(tmpA,3));
-		// PA2 -> PC6
-		//tmpC = SetBit(tmpC, 6, GetBit(tmpA,2));
-		// PA1 -> PC5
-		//tmpC = SetBit(tmpC, 5, GetBit(tmpA,1));
-		// PA0 -> PC4
-		//tmpC = SetBit(tmpC, 4, GetBit(tmpA,0));
 		PORTB = tmpB;
 		PORTC = tmpC;
+		
+	// PA7 -> PB3
+	//tmpB = SetBit(tmpB, 3, GetBit(tmpA,7));
+	// PA6 -> PB2
+	//tmpB = SetBit(tmpB, 2, GetBit(tmpA,6));
+	// PA5 -> PB1
+	//tmpB = SetBit(tmpB, 1, GetBit(tmpA,5));
+	// PA4 -> PB0
+	//tmpB = SetBit(tmpB, 0, GetBit(tmpA,4));
+	// PA3 -> PC7
+	//tmpC = SetBit(tmpC, 7, GetBit(tmpA,3));
+	// PA2 -> PC6
+	//tmpC = SetBit(tmpC, 6, GetBit(tmpA,2));
+	// PA1 -> PC5
+	//tmpC = SetBit(tmpC, 5, GetBit(tmpA,1));
+	// PA0 -> PC4
+	//tmpC = SetBit(tmpC, 4, GetBit(tmpA,0));
     }
 }
 
