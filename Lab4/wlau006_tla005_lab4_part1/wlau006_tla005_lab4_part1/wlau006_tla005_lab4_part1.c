2d581fb5106d90050b0e398888c7bf8516fb3dc8
Tinh La <tla005@ucr.edu>
2019-04-10 16:48:42 -0700

    Wrote shell for Lab4 Part 1

--- Lab4/wlau006_tla005_lab4_part1/wlau006_tla005_lab4_part1/wlau006_tla005_lab4_part1.c
+++ Lab4/wlau006_tla005_lab4_part1/wlau006_tla005_lab4_part1/wlau006_tla005_lab4_part1.c
@@ -8,7 +8,86 @@
 
 #include <avr/io.h>
 
+//global variables
+unsigned char tmpA;
+unsigned char tmpB;
 
+enum States {Start, Init, First, FirstPress, Second, SecondPress} state;
+
+// Bit-access function
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b){
+	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+}
+unsigned char GetBit(unsigned char x, unsigned char k) {
+	return ((x & (0x01 << k)) != 0);
+}
+
+#define button GetBit(tmpA, 0);
+
+void state_func() {
+	switch (state){ // Transitions
+		case Start:
+			state = Init;	
+			break;
+		case Init:
+			state = First;
+			break;
+		case First:
+			if(button){
+				state = FirstPress;
+			}else{
+				state = First;
+			}
+			break;
+		case FirstPress:
+			if(!button){
+				state = Second;
+			}else{
+				state = FirstPress;
+			}
+			break;
+		case Second:
+			if(button){
+				state = SecondPress;
+				}else{
+					state = Second;
+			}
+			break;
+		case SecondPress:
+			if(!button){
+				state = First;
+				}else{
+					state = SecondPress;
+				}
+				break;
+		default:
+			state = Init;
+			break;
+	} 
+	
+	switch (state) { // Actions
+		case Start:
+		
+		break;
+		case Init:
+		
+		break;
+		case First:
+		
+		break;
+		case FirstPress:
+		
+		break;
+		case Second:
+		
+		break;
+		case SecondPress:
+		
+		break;
+		default:
+		break;
+	}
+}
 int main(void)
 {
     /* Replace with your application code */
