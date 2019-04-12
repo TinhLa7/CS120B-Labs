3137df1599c24c724abb3775939bd7f755f1c5f6
Tinh La <tla005@ucr.edu>
2019-04-11 21:00:13 -0700

    exercise 4

--- Lab4/wlau006_tla005_lab4_part4/wlau006_tla005_lab4_part4/main.c
+++ Lab4/wlau006_tla005_lab4_part4/wlau006_tla005_lab4_part4/main.c
@@ -1,7 +1,7 @@
 /*	Partner 1 Name & E-mail: Wyland Lau, wlau006@ucr.edu
  *	Partner 2 Name & E-mail: Tinh La, tla005@ucr.edu
  *	Lab Section: 025
- *	Assignment: Lab 4  Exercise 3
+ *	Assignment: Lab 4  Exercise 4
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
