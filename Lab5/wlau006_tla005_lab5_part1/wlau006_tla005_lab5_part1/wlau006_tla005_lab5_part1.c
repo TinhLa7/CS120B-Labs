f8942dd4a95a56dc8e0414debf45b13db785b427
Tinh La <tla005@ucr.edu>
2019-04-16 09:58:22 -0700

    lab5

---
+++ Lab5/wlau006_tla005_lab5_part1/wlau006_tla005_lab5_part1/wlau006_tla005_lab5_part1.c
@@ -0,0 +1,44 @@
+/*	Partner 1 Name & E-mail: Wyland Lau, wlau006@ucr.edu
+ *	Partner 2 Name & E-mail: Tinh La, tla005@ucr.edu
+ *	Lab Section: 025
+ *	Assignment: Lab 5 Exercise 1 
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+
+
+#include <avr/io.h>
+
+unsigned char GetBit(unsigned char x, unsigned char k) {
+	return ((x & (0x01 << k)) != 0);
+}
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A	
+	while(1)
+	{
+		// 1) Read input
+		tmpA = PINA & 0xFF;
+		//2) Perform computation
+		if(tmpA == 0){
+			PORTC = (PORTC & 0x00) | 0x40;
+		}else if(tmpA >=1 && tmpA <= 2){
+			PORTC = (PORTC & 0x00) | 0x60;	
+		}else if(tmpA >= 3 && tmpA <= 4){
+			PORTC = (PORTC & 0x00) | 0x70;
+		}else if(tmpA >= 5 && tmpA <= 6){
+			PORTC = (PORTC & 0x00) | 0x38;
+		}else if(tmpA >= 7 && tmpA <= 9){
+			PORTC = (PORTC & 0x00) | 0x3C;
+		}else if(tmpA >= 10 && tmpA <= 12){
+			PORTC = (PORTC & 0x00) | 0x3E;
+		}else if(tmpA >= 13 && tmpA <= 15){
+			PORTC = (PORTC & 0x00) | 0x3F;
+		}
+	}
+	return 0;
+}
