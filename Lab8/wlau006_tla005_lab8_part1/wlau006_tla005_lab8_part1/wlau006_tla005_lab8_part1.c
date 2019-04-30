02786f2209c9f3c2f352059db166b4dcdbedd78f
Tinh La <tla005@ucr.edu>
2019-04-30 14:37:15 -0700

    Create Lab 8

---
+++ Lab8/wlau006_tla005_lab8_part1/wlau006_tla005_lab8_part1/wlau006_tla005_lab8_part1.c
@@ -0,0 +1,19 @@
+/*	Partner 1 Name & E-mail: Wyland Lau, wlau006@ucr.edu
+ *	Partner 2 Name & E-mail: Tinh La, tla005@ucr.edu
+ *	Lab Section: 025
+ *	Assignment: Lab 8  Exercise 1
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
