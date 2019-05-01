055c5f34d90bcdf1113c6cac0d2aac2926766a12
Tinh La <tla005@ucr.edu>
2019-04-30 21:19:43 -0700

    Implemented timer

--- Lab8/wlau006_tla005_lab8_part4/wlau006_tla005_lab8_part4/wlau006_tla005_lab8_part4.c
+++ Lab8/wlau006_tla005_lab8_part4/wlau006_tla005_lab8_part4/wlau006_tla005_lab8_part4.c
@@ -6,7 +6,26 @@
  *	code, is my own original work.
  */
 
+/*	Partner 1 Name & E-mail: Wyland Lau, wlau006@ucr.edu
+ *	Partner 2 Name & E-mail: Tinh La, tla005@ucr.edu
+ *	Lab Section: 025
+ *	Assignment: Lab 8  Exercise 3
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+unsigned short x; 	unsigned char tmpB = 0;
+
+// From Exercise 2
+const unsigned short MAX;
 
 void ADC_init() {
 	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
@@ -17,46 +36,104 @@ void ADC_init() {
 	//        the previous conversion completes.
 }
 
+
+void TimerOn() {
+	// AVR timer/counter controller register TCCR1
+	TCCR1B = 0x0B;// bit3 = 0: CTC mode (clear timer on compare)
+	// bit2bit1bit0=011: pre-scaler /64
+	// 00001011: 0x0B
+	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+	// Thus, TCNT1 register will count at 125,000 ticks/s
+
+	// AVR output compare register OCR1A.
+	OCR1A = 125;	// Timer interrupt will be generated when TCNT1==OCR1A
+	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
+	// So when TCNT1 register equals 125,
+	// 1 ms has passed. Thus, we compare to 125.
+	// AVR timer interrupt mask register
+	TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
+	TCNT1=0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+
+	//Enable global interrupts
+	SREG |= 0x80; // 0x80: 1000000
+}
+
+void TimerOff() {
+	TCCR1B = 0x00; // bit3bit1bit0=000: timer off
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect) {
+	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
+		TimerISR(); // Call the ISR that the user uses
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
 // from part 2
-const unsigned char MAX;
+const unsigned short MAX;
+// MAX divided by eight
+const unsigned short threshold;
+
+void tick_func(){
+	x = ADC;
+	if( x > 0 && x <= threshold){
+		tmpB = 0x01;
+	}
+	else if(x > threshold && x <= (2*threshold)){
+		tmpB = 0x03;
+	}
+	else if(x > (2*threshold) && x <= (3*threshold)){
+		tmpB = 0x07;
+	}
+	else if(x > (3*threshold) && x <= (4*threshold)){
+		tmpB = 0x0F;
+	}
+	else if(x > (4*threshold) && x <= (5*threshold)){
+		tmpB = 0x1F;
+	}
+	else if(x > (5*threshold) && x <= (6*threshold)){
+		tmpB = 0x3F;
+	}
+	else if(x > (6*threshold) && x <= (7*threshold)){
+		tmpB = 0x7F;
+	}
+	else if(x > (7*threshold) && x <= (8*threshold)){
+		tmpB = 0xFF;
+	}
+}
 
 int main(void)
 {
 	ADC_init();
-	unsigned short x = ADC;  // Value of ADC register now stored in variable x.
-	unsigned short threshold = MAX / 8;
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
-	unsigned char tmpB = 0;
-	while (1)
-	{
-		if( x > 0 && x <= threshold){
-			tmpB = 0x01;
-		}
-		else if(x > threshold && x <= (2*threshold)){
-			tmpB = 0x03;
-		}
-		else if(x > (2*threshold) && x <= (3*threshold)){
-			tmpB = 0x07;
-		}
-		else if(x > (3*threshold) && x <= (4*threshold)){
-			tmpB = 0x0F;
-		}
-		else if(x > (4*threshold) && x <= (5*threshold)){
-			tmpB = 0x1F;
-		}
-		else if(x > (5*threshold) && x <= (6*threshold)){
-			tmpB = 0x3F;
-		}
-		else if(x > (6*threshold) && x <= (7*threshold)){
-			tmpB = 0x7F;
-		}
-		else if(x > (7*threshold) && x <= (8*threshold)){
-			tmpB = 0xFF;
-		}
+	TimerSet(500); // Set timer here
+	TimerOn();
+    while (1) 
+    {
+		tick_func();
 		PORTB = tmpB;
-		x = ADC
-	}
+		while(!TimerFlag);
+		TimerFlag = 0;
+    }
 }
 
 
