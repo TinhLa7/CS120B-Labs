5b50f90912c176a1968a9d8b1ba189f98c2eb6ee
TinhLa7 <43663011+TinhLa7@users.noreply.github.com>
2019-05-05 16:48:38 -0700

    Create Lab 9 Exercise

---
+++ Lab9/wlau006_tla005_lab9_part1/wlau006_tla005_lab9_part1/main.c
@@ -0,0 +1,18 @@
+/*
+ * wlau006_tla005_lab9_part1.c
+ *
+ * Created: 5/5/2019 4:47:33 PM
+ * Author : tinhl
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
