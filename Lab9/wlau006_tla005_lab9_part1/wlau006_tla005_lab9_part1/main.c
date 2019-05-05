07e96aacba55c294268dfa801b5687766068cb00
TinhLa7 <43663011+TinhLa7@users.noreply.github.com>
2019-05-05 16:57:13 -0700

    Update main.c

--- Lab9/wlau006_tla005_lab9_part1/wlau006_tla005_lab9_part1/main.c
+++ Lab9/wlau006_tla005_lab9_part1/wlau006_tla005_lab9_part1/main.c
@@ -1,12 +1,18 @@
-/*
- * wlau006_tla005_lab9_part1.c
- *
- * Created: 5/5/2019 4:47:33 PM
- * Author : tinhl
- */ 
+/*	Partner 1 Name & E-mail: Wyland Lau, wlau006@ucr.edu
+ *	Partner 2 Name & E-mail: Tinh La, tla005@ucr.edu
+ *	Lab Section: 025
+ *	Assignment: Lab 9  Exercise 1
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
 
 #include <avr/io.h>
-
+#define button1 (PINA & 0x01)
+#define button2 (PINA & 0x20)
+#define button3 (PINA & 0x20)
+#define C4 (261.63)
+#define D4 (293.66)
+#define E4 (329.63)
 
 int main(void)
 {
