25174579738f9aea2a29f7547a465e85a0e2b8e2
Tinh La <tla005@ucr.edu>
2019-05-06 09:25:54 -0700

    Change

--- Lab9.1/wlau006_tla005_lab9_part2/wlau006_tla005_lab9_part2/main.c
+++ Lab9.1/wlau006_tla005_lab9_part2/wlau006_tla005_lab9_part2/main.c
@@ -7,45 +7,104 @@
  */
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+unsigned char tmpB = 0x00;
+void TimerOn() {
+	// AVR timer/counter controller register TCCR1
+	TCCR1B = 0x0B;// bit3 = 0: CTC mode (clear timer on compare)
+	// bit2bit1bit0=011: pre-scaler /64
+	// 00001011: 0x0B
+	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+	// Thus, TCNT1 register will count at 125,000 ticks/s
+
+	// AVR output compare register OCR1A.
+	OCR1A = 125;	// Timer interrupt will be generated when TCNT1==OCR1A
+	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
+	// So when TCNT1 register equals 125,
+	// 1 ms has passed. Thus, we compare to 125.
+	// AVR timer interrupt mask register
+	TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
+	TCNT1=0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+
+	//Enable global interrupts
+	SREG |= 0x80; // 0x80: 1000000
+}
+
+void TimerOff() {
+	TCCR1B = 0x00; // bit3bit1bit0=000: timer off
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect) {
+	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
+		TimerISR(); // Call the ISR that the user uses
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
 
 // 0.954 hz is lowest frequency possible with this function,
 // based on settings in PWM_on()
 // Passing in 0 as the frequency will stop the speaker from generating sound
-void set_PWM(double frequency) {
-	static double current_frequency; // Keeps track of the currently set frequency
+void set_PWM(double frequency) { static double current_frequency;
+	// Keeps track of the currently set frequency
 	// Will only update the registers when the frequency changes, otherwise allows
 	// music to play uninterrupted.
 	if (frequency != current_frequency) {
-		if (!frequency) { TCCR0B &= 0x08; } //stops timer/counter
-		else { TCCR0B |= 0x03; } // resumes/continues timer/counter
-		
+		if (!frequency) {
+			TCCR3B &= 0x08;
+		} //stops timer/counter
+		else {
+			TCCR3B |= 0x03;
+		} // resumes/continues timer/counter
 		// prevents OCR3A from overflowing, using prescaler 64
 		// 0.954 is smallest frequency that will not result in overflow
-		if (frequency < 0.954) { OCR0A = 0xFFFF; }
-		
-		// prevents OCR0A from underflowing, using prescaler 64					// 31250 is largest frequency that will not result in underflow
-		else if (frequency > 31250) { OCR0A = 0x0000; }
-		
-		// set OCR3A based on desired frequency
-		else { OCR0A = (short)(8000000 / (128 * frequency)) - 1; }
-
-		TCNT0 = 0; // resets counter
+		if (frequency < 0.954) {
+			OCR3A = (int)0xFFFF;
+		} // prevents OCR0A from underflowing, using prescaler 64
+		// 31250 is largest frequency that will not result in underflow
+		else if (frequency > 31250) {
+			OCR3A = 0x0000;
+		} // set OCR3A based on desired frequency
+		else {
+			OCR3A = (short)(8000000 / (128 * frequency)) - 1;
+		}
+		TCNT3 = 0; // resets counter
 		current_frequency = frequency; // Updates the current frequency
 	}
 }
-
 void PWM_on() {
-	TCCR0A = (1 << COM0A0);
-	// COM3A0: Toggle PB3 on compare match between counter and OCR0A
-	TCCR0B = (1 << WGM02) | (1 << CS01) | (1 << CS00);
-	// WGM02: When counter (TCNT0) matches OCR0A, reset counter
+	TCCR3A = (1 << COM3A0); // COM3A0: Toggle PB3 on compare match between counter and OCR0A
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30); // WGM02: When counter (TCNT0) matches OCR0A, reset counter
 	// CS01 & CS30: Set a prescaler of 64
 	set_PWM(0);
 }
 
 void PWM_off() {
-	TCCR0A = 0x00;
-	TCCR0B = 0x00;
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
 }
 unsigned char tmpA;
 enum tone_states{ tone_init, tone_1, tone_2, tone_3, tone_4, tone_5, tone_6, tone_7, tone_8} tone_state;
@@ -136,7 +195,7 @@ void syncSM1(){
 			}
 		break;
 		
-		case default:
+		default:
 			tone_state = tone_1;
 		break;
 	}
@@ -176,7 +235,7 @@ void syncSM1(){
 			set_PWM(523.25);
 		break;
 		
-		case default:
+		default:
 		break;
 	}
 	
@@ -219,29 +278,27 @@ void syncSM2(){
 			}
 		break;
 		
-		case default:
+		default:
 			power = off_2;
 		break;
 	}
 	
 	switch(power){
 		case on_1:
-		PWM_on();
 		break;
 		case on_2:
-		PWM_on();
 		break;
 		
 		case off_1:
-		PWM_off();
+		set_PWM(0);
 		break;
 		
 		case off_2:
-		PWM_off();
+		set_PWM(0);
 		break;
 		
-		case default:
-		PWM_off();
+		default:
+		set_PWM(0);
 		break;
 	}
 }
@@ -252,10 +309,14 @@ int main(void)
 	DDRA = 0x00; PORTA = 0xFF;
     /* Replace with your application code */
 	tone_state = tone_init;
-    while (1) 
-    {
+	PWM_on();
+	TimerSet(225);
+	TimerOn();
+    while(1){
 		syncSM1();
 		syncSM2();
+		while(!TimerFlag);
+		TimerFlag = 0;
     }
 }
 
