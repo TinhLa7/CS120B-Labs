499ab04a74625783f5a724a77a4f22ebd6bb36f1
wlau006 <wlau006@users.noreply.github.com>
2019-05-15 09:35:32 -0700

    add  more stuff

--- Lab11/wlau006_tla005_lab11_part4/wlau006_tla005_lab11_part4/main.c
+++ Lab11/wlau006_tla005_lab11_part4/wlau006_tla005_lab11_part4/main.c
@@ -1,7 +1,7 @@
 /*	Partner 1 Name & E-mail: Wyland Lau, wlau006@ucr.edu
  *	Partner 2 Name & E-mail: Tinh La, tla005@ucr.edu
  *	Lab Section: 025
- *	Assignment: Lab 11  Exercise 1
+ *	Assignment: Lab 11  Exercise 2
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
@@ -11,6 +11,10 @@
 #include <timer.h>
 #include <stdio.h>
 
+
+unsigned char writeFlag;
+
+volatile unsigned char x;
 
 typedef struct _task {
 	/*Tasks should have members that include: state, period,
@@ -33,40 +37,6 @@ PC1 2   4 | 5 | 6 | B
 PC2 3   7 | 8 | 9 | C
 PC3 4   * | 0 | # | D
 */
-unsigned char GetKeypadKey() {
-	PORTC = 0x7F;
-	asm("nop");
-	if (GetBit(PINC,0)==0) { return('A'); }
-	if (GetBit(PINC,1)==0) { return('3'); }
-	if (GetBit(PINC,2)==0) { return('2'); }
-	if (GetBit(PINC,3)==0) { return('1'); }
-	
-	// Check keys in col 3
-	PORTC = 0xBF; // Enable col 6 with 0, disable others with 1’s
-	asm("nop"); // add a delay to allow PORTC to stabilize before checking
-	if (GetBit(PINC,0)==0) { return('B'); }
-	if (GetBit(PINC,1)==0) { return('6'); }
-	if (GetBit(PINC,2)==0) { return('5'); }
-	if (GetBit(PINC,3)==0) { return('4'); }
-	
-	// Check keys in col 2
-	PORTC = 0xDF; // Enable col 5 with 0, disable others with 1’s
-	asm("nop"); // add a delay to allow PORTC to stabilize before checking
-	if (GetBit(PINC,0)==0) { return('C'); }
-	if (GetBit(PINC,1)==0) { return('9'); }
-	if (GetBit(PINC,2)==0) { return('8'); }
-	if (GetBit(PINC,3)==0) { return('7'); }
-		
-	PORTC = 0xEF; // Enable col 4 with 0, disable others with 1’s
-	asm("nop"); // add a delay to allow PORTC to stabilize before checking
-	if (GetBit(PINC,0)==0) { return('D'); }
-	if (GetBit(PINC,1)==0) { return('#'); }
-	if (GetBit(PINC,2)==0) { return('0'); }
-	if (GetBit(PINC,3)==0) { return('*'); }
-
-	return('\0'); // default value
-
-}
 
 unsigned long int findGCD(unsigned long int a, unsigned long int b)
 {
@@ -79,8 +49,43 @@ unsigned long int findGCD(unsigned long int a, unsigned long int b)
 	}
 	return 0;
 }
+unsigned char GetKeypadKey() {
 
-enum Output_States {Init, Display};
+	PORTC = 0xEF; // Enable col 4 with 0, disable others with 1’s
+	asm("nop"); // add a delay to allow PORTC to stabilize before checking
+	if (GetBit(PINC,0)==0) { return('D'); }
+	if (GetBit(PINC,1)==0) { return('#'); }
+	if (GetBit(PINC,2)==0) { return('0'); }
+	if (GetBit(PINC,3)==0) { return('*'); }
+
+	// Check keys in col 2
+	PORTC = 0xDF; // Enable col 5 with 0, disable others with 1’s
+	asm("nop"); // add a delay to allow PORTC to stabilize before checking
+	if (GetBit(PINC,0)==0) { return('C'); }
+	if (GetBit(PINC,1)==0) { return('9'); }
+	if (GetBit(PINC,2)==0) { return('8'); }
+	if (GetBit(PINC,3)==0) { return('7'); }
+
+	// Check keys in col 3
+	PORTC = 0xBF; // Enable col 6 with 0, disable others with 1’s
+	asm("nop"); // add a delay to allow PORTC to stabilize before checking
+	if (GetBit(PINC,0)==0) { return('B'); }
+	if (GetBit(PINC,1)==0) { return('6'); }
+	if (GetBit(PINC,2)==0) { return('5'); }
+	if (GetBit(PINC,3)==0) { return('4'); }
+
+	// Check keys in col 4
+	PORTC = 0x7F;
+	asm("nop");
+	if (GetBit(PINC,0)==0) { return('A'); }
+	if (GetBit(PINC,1)==0) { return('3'); }
+	if (GetBit(PINC,2)==0) { return('2'); }
+	if (GetBit(PINC,3)==0) { return('1'); }
+
+	return('\0'); // default value
+
+}
+/*enum Output_States {Init, Assign};
 	
 int Output_tick(int state){
 	switch(state){ //Transitions
@@ -94,26 +99,44 @@ int Output_tick(int state){
 	}
 	switch(state){ //Actions
 		case Init: break;
-		case Display:
-			
+		case Assign:
+		if (writeFlag)
+		{
+			LCD_WriteData(x); writeFlag = 0;
+		}
+ // All 5 LEDs on
+		break;
 	}
 	return state;
-}
+}*/
 
-enum Keypad_States{Keypad_init, Keypad_read};
-	
+enum Keypad_States{Keypad_init, Keypad_read, Keypad_wait};
+
 int Keypad_tick(int state){
 	switch(state){ //Transitions
 		case Keypad_init:
-			state = Keypad_read;
+		state = Keypad_read;
 		break;
 		case Keypad_read:
+		if(x != '\0'){
+			state = Keypad_wait;
+		}else{
 			state = Keypad_read;
+		}
+			//state = (x != '\0') ? Keypad_wait: Keypad_read;
+		break;
+		case Keypad_wait:
+		if(GetKeypadKey() != '\0'){
+			state = Keypad_wait;
+			}else{
+			state = Keypad_read;
+		}
 		break;
 	}
 	switch(state){
 		case Keypad_init: break;
-		case Keypad_read: x = GetKeypadKey(); break;
+		case Keypad_read: x = GetKeypadKey(); if(x != '\0'){ LCD_WriteData(x);} break;
+		case Keypad_wait: break;
 		default: break;
 	}
 	return state;
@@ -121,31 +144,33 @@ int Keypad_tick(int state){
 	
 int main(void)
 {
-	unsigned long int read_calc = 50;
-	unsigned long int output_calc = 100;
+	unsigned long int scroll_calc = 50;
+	//unsigned long int output_calc = 200;
 	unsigned long int tmpGCD = 0;
-	tmpGCD = findGCD(read_calc, output_calc);
+	tmpGCD = findGCD(scroll_calc, scroll_calc);//, output_calc);
 	unsigned long int GCD = tmpGCD;
-	unsigned long int read_period = read_calc/GCD;
-	unsigned long int output_period = output_calc/GCD;
+	unsigned long int scroll_period = scroll_calc/GCD;
+	//unsigned long int output_period = output_calc/GCD;
 	
-	static task task1, task2;
-	task *tasks[] = { &task1, &task2};
+	static task task1;//, task2;
+	task *tasks[] = { &task1};//, &task2};
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 	
 	unsigned char i;
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRD = 0xFF; PORTD = 0x00; // PORTD set to output, outputs init 0s
 	DDRC = 0xF0; PORTC = 0x0F; // PC7..4 outputs init 0s, PC3..0 inputs init 1
-	task1.state = Init;
-	task1.period = read_period;
-	task1.elapsedTime = read_period;
+	task1.state = Keypad_init;
+	task1.period = scroll_period;
+	task1.elapsedTime = scroll_period;
 	task1.TickFct = &Keypad_tick;
-	task2.state = Keypad_init;
+	/*task2.state = Init;
 	task2.period = output_period;
 	task2.elapsedTime = output_period;
-	task2.TickFct = &Output_tick;
+	task2.TickFct = &Output_tick;*/
 	LCD_init();
+	LCD_DisplayString(1,"Congratulations!");
+	LCD_Cursor(1);
 	TimerSet(GCD);
 	TimerOn();
 //			LCD_DisplayString(1, "Hello World");
