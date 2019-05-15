dc8ea5b8777ae5a7ef072dea58f0a29fe6098872
wlau006 <wlau006@users.noreply.github.com>
2019-05-15 07:57:22 -0700

    moved

--- Lab11/wlau006_tla005_lab11_part1/wlau006_tla005_lab11_part1/wlau006_tla005_lab11_part1.c
+++ Lab11/wlau006_tla005_lab11_part1/wlau006_tla005_lab11_part1/wlau006_tla005_lab11_part1.c
@@ -35,22 +35,13 @@ PC2 3   7 | 8 | 9 | C
 PC3 4   * | 0 | # | D
 */
 unsigned char GetKeypadKey() {
-
-	PORTC = 0xEF; // Enable col 4 with 0, disable others with 1’s
-	asm("nop"); // add a delay to allow PORTC to stabilize before checking
-	if (GetBit(PINC,0)==0) { return('D'); }
-	if (GetBit(PINC,1)==0) { return('#'); }
-	if (GetBit(PINC,2)==0) { return('0'); }
-	if (GetBit(PINC,3)==0) { return('*'); }
-
-	// Check keys in col 2
-	PORTC = 0xDF; // Enable col 5 with 0, disable others with 1’s
-	asm("nop"); // add a delay to allow PORTC to stabilize before checking
-	if (GetBit(PINC,0)==0) { return('C'); }
-	if (GetBit(PINC,1)==0) { return('9'); }
-	if (GetBit(PINC,2)==0) { return('8'); }
-	if (GetBit(PINC,3)==0) { return('7'); }
-
+	PORTC = 0x7F;
+	asm("nop");
+	if (GetBit(PINC,0)==0) { return('A'); }
+	if (GetBit(PINC,1)==0) { return('3'); }
+	if (GetBit(PINC,2)==0) { return('2'); }
+	if (GetBit(PINC,3)==0) { return('1'); }
+	
 	// Check keys in col 3
 	PORTC = 0xBF; // Enable col 6 with 0, disable others with 1’s
 	asm("nop"); // add a delay to allow PORTC to stabilize before checking
@@ -58,14 +49,21 @@ unsigned char GetKeypadKey() {
 	if (GetBit(PINC,1)==0) { return('6'); }
 	if (GetBit(PINC,2)==0) { return('5'); }
 	if (GetBit(PINC,3)==0) { return('4'); }
-
-	// Check keys in col 4	
-	PORTC = 0x7F;
-	asm("nop");
-	if (GetBit(PINC,0)==0) { return('A'); }
-	if (GetBit(PINC,1)==0) { return('3'); }
-	if (GetBit(PINC,2)==0) { return('2'); }
-	if (GetBit(PINC,3)==0) { return('1'); }
+	
+	// Check keys in col 2
+	PORTC = 0xDF; // Enable col 5 with 0, disable others with 1’s
+	asm("nop"); // add a delay to allow PORTC to stabilize before checking
+	if (GetBit(PINC,0)==0) { return('C'); }
+	if (GetBit(PINC,1)==0) { return('9'); }
+	if (GetBit(PINC,2)==0) { return('8'); }
+	if (GetBit(PINC,3)==0) { return('7'); }
+		
+	PORTC = 0xEF; // Enable col 4 with 0, disable others with 1’s
+	asm("nop"); // add a delay to allow PORTC to stabilize before checking
+	if (GetBit(PINC,0)==0) { return('D'); }
+	if (GetBit(PINC,1)==0) { return('#'); }
+	if (GetBit(PINC,2)==0) { return('0'); }
+	if (GetBit(PINC,3)==0) { return('*'); }
 
 	return('\0'); // default value
 
